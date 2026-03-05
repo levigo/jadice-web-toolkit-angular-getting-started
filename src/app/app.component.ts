e72d23b2448a571e128cfb6f6205aee01edf2106
Martin Bachl <36962174+bachlmn@users.noreply.github.com>
2026-03-05 09:33:19 +0100

    simple landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'jwv-getting-started';
+
+
+export class AppComponent implements OnInit{
+  ngOnInit() {
+    // do init at here for current route.
+    console.log("hello world");
+    setTimeout(() => {
+      window.location.href = 'http://localhost:8080';
+    }, 5000);  //5s
+  }
 }
